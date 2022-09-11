8568cb04ce2687ef245868a90fc9ca3a4639ef55
Andrii Susenko <susenko.andrii@gmail.com>
2022-09-11 13:42:44 +0300

    add label homeCraft2D and monetki

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var gameName = "Home Craft 2D";
+
 var canvas = document.getElementById("canvas1");
 var ctx = canvas.getContext("2d");
 
@@ -6,6 +8,10 @@ ctx.strokeStyle = "lightgrey";
 var blockSize = 50;
 var rows = 12;
 var columns = 20;
+var blocks = [];
+
+//blocks.push('234sdfsdf');
+//alert (blocks[0]);
 
 for (var j = 0; j < rows; j++) {
     var y = j * blockSize;
@@ -16,3 +22,26 @@ for (var j = 0; j < rows; j++) {
         ctx.stroke();   
     }
 }
+
+
+
+
+var drawNameOfTheGame = function () {
+    ctx.font = "25px Comic Sans MS";
+    ctx.fillStyle = "Grey";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText(gameName, 10, 10)
+  };
+
+  drawNameOfTheGame();
+
+var monetki = 0;  
+  var drawCoins = function () {
+    ctx.font = "25px Arial";
+    ctx.fillStyle = "orange";
+    ctx.textAlign = "right";
+    ctx.textBaseline = "top";
+    ctx.fillText("Monetki: " + monetki, 1000, 10);
+  };
+  drawCoins();
