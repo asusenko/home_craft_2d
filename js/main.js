813cb46d38353ed0de271f77017c63230d91cc6b
Andrii Susenko <susenko.andrii@gmail.com>
2022-09-11 13:08:46 +0300

    draw simple block

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//alert(1);
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.rect(0, 0, 50,50);
+ctx.stroke();
+
+//  ctx.strokeStyle = "grey";
+//  for (var i = 0; i < 20; i++){
+//      var x = 
+//      ctx.beginPath();
+//      ctx.rect(50 * (0 * i), 0, 50 + ,50);
+//      ctx.stroke();   
+//  }
